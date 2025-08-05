0c4b48d0d3976c811fc2bdb025d0fb2b676c0471
Batiste32 <batiste32.augereau@gmail.com>
2025-08-05 14:21:44 +0200

    Restricted CORS to allow for credentials

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ def run_fastapi_app():
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],
+        allow_origins=["https://mapper-frontend.onrender.com","https://localhost:8000"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
