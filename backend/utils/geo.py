870ba32172fca9584006776138c317844edecc8c
Batiste32 <batiste32.augereau@gmail.com>
2025-08-12 13:34:59 +0200

    Enforcing max cluster size instead of relying on KMeans

--- backend/utils/geo.py
+++ backend/utils/geo.py
@@ -190,6 +190,11 @@ def get_directions_route(ordered_points):
     print(f"Original points : {ordered_points[:10]}...{ordered_points[-10:]}")
     print(f"Rearranged points : {coordinates[:10]}...{coordinates[-10:]}")
 
+    for lon,lat in coordinates :
+        if (lon > -70) or (lat < 40):
+            print(f"WARN coordinates seem outside of expected value :\nlon: {lon}, lat: {lat}")
+
+
     body = {
         "coordinates": coordinates,
         "instructions": True
