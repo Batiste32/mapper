c8afc28fdf697cddf7eff7a9144afa33654c91b5
Batiste32 <batiste32.augereau@gmail.com>
2025-08-18 22:39:41 +0200

    Moved from Google Drive to Dropbox to store user files

--- backend/database/__init__.py
+++ backend/database/__init__.py
@@ -11,3 +11,4 @@ def set_database_path(db_path: str):
     global engine, SessionLocal
     engine = create_engine(f"sqlite:///{db_path}", connect_args={"check_same_thread": False})
     SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    print(f"Switched database to {db_path}.")
