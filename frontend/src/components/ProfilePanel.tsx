250c497dfbe4b2143c855e0531428abcce48b190
Batiste32 <batiste32.augereau@gmail.com>
2025-09-02 17:34:34 +0200

    Fixed Profile Panel not taking the entire screen

--- frontend/src/components/ProfilePanel.tsx
+++ frontend/src/components/ProfilePanel.tsx
@@ -65,8 +65,7 @@ export default function ProfilePanel({ selectedProfile }: ProfilePanelProps) {
   return (
     <div
       className={`w-full p-4 bg-midnight border-t sm:border-t-0 sm:border-l max-h-[30vh] sm:max-h-none overflow-auto z-10
-        transition-all duration-300 ease-in-out
-        ${selectedProfile ? "sm:w-1/2" : "sm:w-64"}`}
+        transition-all duration-300 ease-in-out`}
     >
       <h2 className="text-xl font-semibold mb-4">Profile Info</h2>
       {selectedProfile ? (
